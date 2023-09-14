d5db6d47e4e92369d19b764e2b2df6564217b501
hbhardwaj <hbhardwaj@tenable.com>
2023-09-15 08:46:42 +1000

    making cluster private

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -4,25 +4,70 @@ provider "google" {
   region      = var.region
 }
 
+
+# create VPC
+resource "google_compute_network" "vpc" {
+  name                    = "vpc1"
+  auto_create_subnetworks = false
+}
+
+# Create Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet1"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.0.0/24"
+}
+
 resource "google_container_cluster" "primary" {
   name               = "gke-test-cluster"
   location           = var.region
+  network                  = google_compute_network.vpc.name
+  subnetwork               = google_compute_subnetwork.subnet.name
+  remove_default_node_pool = true  
   initial_node_count = 1
 
-  enable_private_nodes   = true
-  enable_private_endpoint = true
+  private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes   = true 
+    master_ipv4_cidr_block = "10.13.0.0/28"
+  }
+  
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.11.0.0/21"
+    services_ipv4_cidr_block = "10.12.0.0/21"
+  }
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.7/32"
+      display_name = "net1"
+    }
+
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
 
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/devstorage.read_only",
     ]
+
+    labels = {
+      env = "dev"
+    }
+
+    machine_type = "n1-standard-1"
+    preemptible  = true
+    #service_account = google_service_account.mysa.email
     disk_size_gb = 10
     metadata = {
       disable-legacy-endpoints = "true"
     }
-
-    tags = ["my-gke-node"]
   }
 }
