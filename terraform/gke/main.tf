24eeeb8c3f69b52fdca93abe1a2973ff86d163fb
hbhardwaj <hbhardwaj@tenable.com>
2023-09-15 00:43:36 +1000

    test cluster

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -5,18 +5,10 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name               = "my-gke-cluster"
+  name               = "gke-test-cluster"
   location           = var.region
   initial_node_count = 1
 
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
 
   node_config {
     oauth_scopes = [
