cf3b4df07a927931bc5d11f8a45944478f1c2cd6
hbhardwaj <hbhardwaj@tenable.com>
2023-09-15 09:40:55 +1000

    update

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -53,32 +53,9 @@ resource "google_container_cluster" "primary" {
   }
   node_config {
     disk_size_gb = 10
-  }
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = google_container_cluster.primary.name
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = 3
-  
-
-  node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
-
-    labels = {
-      env = "dev"
-    }
-
-    machine_type = "e2-small"
-    preemptible  = true
-    #service_account = google_service_account.mysa.email
-    disk_size_gb = 10
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
   }
 }
