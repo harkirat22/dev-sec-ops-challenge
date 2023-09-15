1c6b0831915b80fe368ef27283c28de82e88bf65
hbhardwaj <hbhardwaj@tenable.com>
2023-09-15 12:34:39 +1000

    kub config step

--- terraform/gke/output.tf
+++ terraform/gke/output.tf
@@ -2,3 +2,13 @@ output "cluster_endpoint" {
   description = "The IP address of the cluster master."
   value       = google_container_cluster.primary.endpoint
 }
+
+output "cluster_name" {
+  value = google_container_cluster.primary.name
+  description = "The name of the GKE cluster."
+}
+
+output "cluster_zone" {
+  value = google_container_cluster.primary.zone
+  description = "The zone of the GKE cluster."
+}
