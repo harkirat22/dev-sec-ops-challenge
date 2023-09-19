4c324397647415456548db827bb09c55c882132d
hbhardwaj <hbhardwaj@tenable.com>
2023-09-19 16:23:28 +1000

    update

--- terraform/suricata/main.tf
+++ terraform/suricata/main.tf
@@ -24,16 +24,14 @@ resource "kubernetes_daemonset" "suricata" {
       spec {
         init_container {
           name    = "init-network-setup"
-          image   = "busybox:latest"
-          command = ["/bin/sh", "-c", "interface=$(ip -o -4 route show to default | awk '{print $5}') && echo $interface > /tmp/interface-name && ip link set $interface promisc on"]
+          image   = "busybox:latest"  # Using busybox for lightweight shell operations.
+          command = ["/bin/sh", "-c", "interface=$(ip -o -4 route show to default | awk '{print $5}') && ip link set $interface promisc on"]
 
           security_context {
             privileged = true  # Needed to alter network interfaces.
-          }
-
-          volume_mount {
-            name       = "interface-name"
-            mount_path = "/tmp"
+            capabilities {
+              add = ["NET_ADMIN", "NET_RAW", "SYS_NICE"]
+            }
           }
         }
 
@@ -42,17 +40,23 @@ resource "kubernetes_daemonset" "suricata" {
           image = "harkirat101803/custom-suricata:${var.docker_tag}"
 
           args = [
-            "-i", "/tmp/interface-name"
+            "-i", "$(INTERFACE)"
           ]
+
+          env {
+            name  = "INTERFACE"
+            value = ""  # Placeholder value as Kubernetes doesn't directly support extracting arguments from another container.
+          }
+
+          security_context {
+            capabilities {
+              add = ["NET_ADMIN", "NET_RAW", "SYS_NICE"]
+            }
+          }
 
           volume_mount {
             name       = "logs"
             mount_path = "/var/log/suricata"
-          }
-
-          volume_mount {
-            name       = "interface-name"
-            mount_path = "/tmp"
           }
         }
 
@@ -61,11 +65,6 @@ resource "kubernetes_daemonset" "suricata" {
           empty_dir {
             medium = "Memory"
           }
-        }
-
-        volume {
-          name = "interface-name"
-          empty_dir {}
         }
 
         host_network = true
